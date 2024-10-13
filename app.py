2db95a78cbeae31f193357ac88613885c2b6bbd2
Ilyas Khallouki <khalloukielias@gmail.com>
2024-10-13 10:38:49 +0100

    Added the option to list all your network interfaces

--- app.py
+++ app.py
@@ -14,9 +14,11 @@ def main():
     subparsers = parser.add_subparsers(dest='command', help="Available commands")
 
     # Command '-ls'
-    parser_ls = subparsers.add_parser('ls', help='List the IP addresses of all the devices connected to the network')
+    parser_ls = subparsers.add_parser('lI', help='List the IP addresses of all the devices connected to the network')
     parser_ls.add_argument('--interface', required=False, type=str, help="Specify an interface")
 
+    parser_ls = subparsers.add_parser('lD', help='List all the network interfaces')
+    
     # # Example Command 2: add
     # parser_add = subparsers.add_parser('add', help='Add two numbers')
     # parser_add.add_argument('num1', type=int, help='First number')
@@ -29,8 +31,10 @@ def main():
     args = parser.parse_args()
 
     # Handling the logic for each command
-    if args.command == 'ls':
+    if args.command == 'lI':
         list_devices(args.interface)
+    elif args.command == 'lD':
+        list_interfaces()
     elif args.command == 'version':
         show_version()
     else:
@@ -39,14 +43,35 @@ def main():
 def list_devices(interface):
     try:
         monitor = NetworkMonitor(interface)
+        devices = monitor.list_connected_devices()
+        for device in devices:
+            print(f"IP: {device['ip']}, MAC: {device['mac']}")
     except Exception as e:
         if e.args == "interface 0":
             print("Unable to detect an interface - please specify one")
         print(e.args)
         exit(1)
-    devices = monitor.list_connected_devices()
-    for device in devices:
-        print(f"IP: {device['ip']}, MAC: {device['mac']}")
+
+
+def list_interfaces():
+    try: 
+        monitor = NetworkMonitor()
+        interfaces = monitor.list_all_interfaces()
+
+        for iface in interfaces:
+            print(f"Interface: {iface['interface']}")
+            print(f"  Status: {'Up' if iface['is_up'] else 'Down'}")
+            if iface['speed']:
+                print(f"  Speed: {iface['speed']} Mbps")
+            if iface['ip_address']:
+                print(f"  IP Address: {iface['ip_address']}")
+            if iface['mac_address']:
+                print(f"  MAC Address: {iface['mac_address']}")
+            print("\n")
+    except Exception as e:
+        # TODO handle exceptions
+        print('An Error has occured', e)
+        exit(1)
 
 # Display the app version
 def show_version():
--- network_monitor.py
+++ network_monitor.py
@@ -1,9 +1,11 @@
 import scapy.all as scapy
 import ipaddress
+import psutil
+import socket
 from utils import Utils
 
 class NetworkMonitor:
-    def __init__(self, interface):
+    def __init__(self, interface=None):
         self.interface = interface
         self.utils = Utils()
 
@@ -37,3 +39,27 @@ class NetworkMonitor:
             devices.append(device)
 
         return devices
+
+    def list_all_interfaces(self):
+        """Lists all network interfaces and their details."""
+        interfaces = psutil.net_if_addrs()
+        stats = psutil.net_if_stats()
+
+        interface_info = []
+        for iface, addr_list in interfaces.items():
+            interface_details = {
+                'interface': iface,
+                'is_up': stats[iface].isup if iface in stats else False,
+                'speed': stats[iface].speed if iface in stats else None,
+                'ip_address': None,
+                'mac_address': None
+            }
+            for addr in addr_list:
+                if addr.family == socket.AF_INET:
+                    interface_details['ip_address'] = addr.address
+                elif addr.family == psutil.AF_LINK:
+                    interface_details['mac_address'] = addr.address
+
+            interface_info.append(interface_details)
+
+        return interface_info
